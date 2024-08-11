09fc4ff48d9afb7fbc0fa4ab55b5c450ad5dba4b
chandrakalamg1989 <chandrakalamgb@gmail.com>
2024-08-11 15:53:35 +0530

    Adding second file (#2)

---
+++ PrintSUm.java
@@ -0,0 +1,10 @@
+public class PrintSUm {
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b=5;
+        int sum = a+b;
+
+        System.out.println("Printing sum"+sum);
+    }
+}
