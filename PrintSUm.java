7c46031b9fd67a4fbc5efb315dca03f3d0bb6e5c
chandrakala <chandrakalamgb@gmail.com>
2024-08-11 16:09:18 +0530

    change in input value

--- PrintSUm.java
+++ PrintSUm.java
@@ -1,10 +1,12 @@
 public class PrintSUm {
 
     public static void main(String[] args) {
-        int a = 0;
+        int a = 2;
         int b=5;
         int sum = a+b;
 
         System.out.println("Printing sum"+sum);
+
+
     }
 }
