4c911ddaf8b8fc079e1c07c1961b2b68b33666da
chandrakalamg1989 <chandrakalamgb@gmail.com>
2024-08-12 15:22:00 +0530

    Feature 2 (#12)
    * Adding first file
    
    * Update File1.txt
    
    Commiting from browser
    
    * Adding second file
    
    * Adding second file (#2)
    
    * change in input value
    
    * Added new String Class (#7)
    
    * Adding second file
    
    * change in input value
    
    * Changes in printSum (#9)
    
    * Adding second file
    
    * change in input value
    
    * change in input value
    
    * change in input value
    
    * change in input value
    
    * Changes in PrintSum

--- PrintSUm.java
+++ PrintSUm.java
@@ -2,10 +2,13 @@ public class PrintSUm {
 
     public static void main(String[] args) {
 
-        int b=5;
         int sum = a+b;
 
         System.out.println("Printing sum"+sum);
+
+        String s = new String("Chandrakala");
+        System.out.println(s.length());
+        System.out.println(s.toUpperCase());
 
     }
 }
