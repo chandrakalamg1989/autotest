bab8018eb8c3022f2edc2ef0f576f84cbb94b220
chandrakala <chandrakalamgb@gmail.com>
2024-08-12 09:18:44 +0530

    change in input value

--- PrintSUm.java
+++ PrintSUm.java
@@ -1,10 +1,16 @@
 public class PrintSUm {
 
     public static void main(String[] args) {
-        int a = 0;
-        int b=5;
+
+
+       int b=5;
+        int a=10;
         int sum = a+b;
 
         System.out.println("Printing sum"+sum);
+
+        String s = new String("Chandrakala");
+        System.out.println(s.length());
+        System.out.println(s.toUpperCase());
     }
 }
