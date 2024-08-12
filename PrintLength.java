345b8b869916630a64d56389433f1c597eb92ab3
chandrakala <chandrakalamgb@gmail.com>
2024-08-12 09:43:01 +0530

    new Class file named PrintLength added

--- PrintLength.java
+++ PrintLength.java
@@ -1,8 +1,8 @@
 public class PrintLength {
 
     public static void main(String[] args) {
-        String s = new String("Chandrakala");
-        System.out.println(s.length());
-        System.out.println(s.toUpperCase());
+         System.out.println("Chandrakala");
+
+
     }
 }
