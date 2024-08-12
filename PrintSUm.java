0d99806dfb16f506cb38d48b9ccb0c46a35eb8ce
chandrakala <chandrakalamgb@gmail.com>
2024-08-12 09:33:14 +0530

    Changes in PrintSum with extra space

--- PrintSUm.java
+++ PrintSUm.java
@@ -2,6 +2,9 @@ public class PrintSUm {
 
     public static void main(String[] args) {
 
+
+       int b=5;
+        int a=10;
         int sum = a+b;
 
         System.out.println("Printing sum"+sum);
